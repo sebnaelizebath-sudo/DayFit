4936b09cc491d9764805b8c87c54381fd463b1fa
sebnaelizebath-sudo <sebnaelizebath@gmail.com>
2026-04-21 21:33:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,4 +100,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
